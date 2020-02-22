af33e18e69bcd0f08fc53b02993a7511dba9d35f
smreinar <stevenreinart123@gmail.com>
2020-02-21 18:59:08 -0700

    javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,37 @@
 //This JS File is linked to index.html for Psychic Game
-let computerGuesses = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z"
-  ];
+let computerGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  let wins = 0;
-  let losses = 0;
-  let guesses = 9;
-  let guessesLeft = 9;
-  let guessedLetters = [];
-  var letterToGuess = null;
+//global varibles
+var computerAnswer = computerGuesses[Math.floor(Math.random() * computerGuesses.length)];
+
+let guessedLetters = [];
+//console.log("Computer Guess: " + computerAnswer)
+//functions for each
+function computerAnswerFun() {
+    document.getElementsByClassName('.computer').style.display = "none";
+};
+
+//reset
+function resetComputerAnswer(){
+    computerAnswer = computerGuesses[Math.floor(Math.random() * computerGuesses.length)];
+    
+}
+//computer guess
+
+//user guess 
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    
+    if (userGuess === computerAnswer) {
+        console.log("correct")
+        resetComputerAnswer()
+        
+    }
+    console.log(userGuess)
+    // console.log("User Guess: " + userGuess)
+    
+}
+
+// if guess false
+
+//if guess true
