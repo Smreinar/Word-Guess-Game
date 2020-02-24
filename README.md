c0fa205fbe8b308fc836f7dfeb7c94003160b987
smreinar <stevenreinart123@gmail.com>
2020-02-23 18:54:27 -0700

    more javascript

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Word-Guess-Game
+A simple game of guess what letter the computer is thinking.
+Choose a letter from A - Z and see if you can guess what the computer is thinking!
+
+created by: Steven Reinart
+
+things left to do:
+-make a if statements declaring lost
+-edit innerhtml for lost
+-make a if statment declaring a letter cannot be pressed twice in one round 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,32 +3,67 @@ let computerGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 
 //global varibles
 var computerAnswer = computerGuesses[Math.floor(Math.random() * computerGuesses.length)];
+var guessedLetters = [];
+var win = 0;
+var lost = 0;
+var lives = 9;
 
-let guessedLetters = [];
-//console.log("Computer Guess: " + computerAnswer)
+
+
+//console.log("Computer Guess: " + computerAnswer);
 //functions for each
+function lettersGuessed() {
+    document.getElementsById("userGuesses").innerHTML = "Your Guesses So Far: " + guessedLetters.join(', ')
+}
+
 function computerAnswerFun() {
     document.getElementsByClassName('.computer').style.display = "none";
 };
 
-//reset
+//reset functions
 function resetComputerAnswer(){
     computerAnswer = computerGuesses[Math.floor(Math.random() * computerGuesses.length)];
     
 }
-//computer guess
+
+function resetRound(){
+    guessedLetters = [];
+    lives = 9;
+}
+
+//Dom
+let guessDom = document.getElementsByClassName("userGuesses");
+let livesDom = document.getElementById("guessesLeft");
+
+
 
 //user guess 
 document.onkeyup = function(event) {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    
+    var wins = win
+     
     if (userGuess === computerAnswer) {
-        console.log("correct")
-        resetComputerAnswer()
+        wins++;
+        alert("Your Guess '" + userGuess + "' Was Correct");
+        guessedLetters.push(userGuess);
+        document.getElementById("wins").innerHTML = "Wins: " + wins;
         
+        resetComputerAnswer();
+           
+    } if (userGuess != computerAnswer){
+        lives--;
+        guessedLetters.push(userGuess);
+        guessDom[0].innerHTML = guessedLetters.join(' ');
+        livesDom.innerHTML = lives;
     }
-    console.log(userGuess)
-    // console.log("User Guess: " + userGuess)
+
+   
+
+
+    
+    
+    console.log(userGuess);
+    // console.log("User Guess: " + userGuess);
     
 }
 
